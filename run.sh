52073ea5cd225c8fea17152cee84d70a0531ef15
YanTee <vanhoinguyen98@gmail.com>
2022-03-31 00:38:17 +0700

    add env and ignore file for docker and git

--- run.sh
+++ run.sh
@@ -5,6 +5,20 @@ if [[ $opt = "up-dev" ]]; then
     docker-compose -f docker-compose.dev.yml up -d
 elif [[ $opt = "down-dev" ]]; then
   docker-compose -f docker-compose.dev.yml down
+elif [[ $opt = "up-dev-build" ]]; then
+  docker-compose -f docker-compose.dev.yml build
+  docker-compose -f docker-compose.dev.yml up --force-recreate -d
+elif [[ $opt = "up-debug" ]]; then
+    docker-compose -f docker-compose.debug.yml up -d
+elif [[ $opt = "down-debug" ]]; then
+  docker-compose -f docker-compose.debug.yml down
+elif [[ $opt = "up-pro" ]]; then
+    docker-compose -f docker-compose.yml up -d
+elif [[ $opt = "down-pro" ]]; then
+  docker-compose -f docker-compose.yml down
+elif [[ $opt = "up-pro-build" ]]; then
+  docker-compose -f docker-compose.yml build
+  docker-compose -f docker-compose.yml up --force-recreate -d
 else
   echo "Invalid command"
 fi
